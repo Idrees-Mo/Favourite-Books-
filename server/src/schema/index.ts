63a2578b42804832a98d99da5321b92f483f10de
muhammad idrees <idris@y7mail.com>
2021-07-24 20:15:24 +0930

    added mutations in Mutations

--- server/src/schema/index.ts
+++ server/src/schema/index.ts
@@ -1,6 +1,6 @@
 import { GraphQLObjectType, GraphQLSchema } from "graphql";
-import { CREATE_AUTHOR } from "./Mutations/author";
-import { CREATE_BOOK } from "./Mutations/book";
+import { CREATE_AUTHOR, REMOVE_AUTHOR } from "./Mutations/author";
+import { CREATE_BOOK, REMOVE_BOOK, UPDATE_BOOK } from "./Mutations/book";
 import { GET_AUTHOR, GET_AUTHORS } from "./Queries/authors";
 import { GET_BOOK, GET_BOOKS } from "./Queries/book";
 
@@ -19,6 +19,9 @@ const Mutations = new GraphQLObjectType({
   fields: {
     createAuthor: CREATE_AUTHOR,
     createBook: CREATE_BOOK,
+    removeAuthor: REMOVE_AUTHOR,
+    removeBook: REMOVE_BOOK,
+    updateBook: UPDATE_BOOK,
   },
 });
 const schema = new GraphQLSchema({
